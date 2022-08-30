f7b2fcc9ad70cd40bfd322de9441a0ecdf61cbaa
gbrad777 <gabe.bradford01@gmail.com>
2022-08-29 23:03:42 -0500

    wip... pushing List for brandon to use in getbyid

--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -1,3 +1,7 @@
+using FA22.P02.Web.Features;
+using System.Reflection.Metadata.Ecma335;
+using static FA22.P02.Web.Features.Products;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -16,13 +20,49 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-app.Run();
-
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+app.MapGet("/api/get-all-products", () =>
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+    return Products;
+});
+
+app.MapPost("/api/create-product", (ProductDto product) =>
+{
+    if (!Products.Where(p => p.Id == product.Id).Any())
+    {
+        Products.Add(product);
+    }
+});
+
+app.Run();
 
 //see: https://docs.microsoft.com/en-us/aspnet/core/test/integration-tests?view=aspnetcore-6.0
 // Hi 383 - this is added so we can test our web project automatically. More on that later
-public partial class Program { }
+public partial class Program 
+{
+    public static List<ProductDto> Products = new List<ProductDto>
+    {
+        new ProductDto
+        {
+            Id = 1,
+            Name = "Controller",
+            Description = "red semi-used controller for xbox",
+            Price = 24.99m,
+        },
+
+        new ProductDto
+        {
+            Id = 2,
+            Name = "Xbox Series X",
+            Description = "newest gen xbox, unopened",
+            Price = 499.99m,
+        },
+
+        new ProductDto
+        {
+            Id = 3,
+            Name = "Halo 5",
+            Description = "Disk is in good condition, no noticeable damage",
+            Price = 14.99m,
+        },
+    };
+}
