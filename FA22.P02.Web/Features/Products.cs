041d4a0ec91ee0f1c27d3f471c4b04e7cd302869
gbrad777 <gabe.bradford01@gmail.com>
2022-08-29 21:07:38 -0500

    wip... pushing starting point to begin splitting up endpoints

---
+++ FA22.P02.Web/Features/Products.cs
@@ -0,0 +1,13 @@
+﻿namespace FA22.P02.Web.Features
+{
+    public class Products
+    {
+        public class ProductDto
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public string Description { get; set; }
+            public decimal? Price { get; set; }
+        } 
+    }
+}
--- FA22.P02.Web/Program.cs
+++ FA22.P02.Web/Program.cs
@@ -16,26 +16,6 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
-app.MapGet("/weatherforecast", () =>
-{
-    throw new Exception("remove me");
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateTime.Now.AddDays(index),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast");
-
 app.Run();
 
 internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
